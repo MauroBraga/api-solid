4465a67dc87afec28824ec186127cd9f1932811c
mauro <mrb0305@gmail.com>
2026-02-22 08:47:19 -0300

    Criando camada de controllers

--- src/app.ts
+++ src/app.ts
@@ -1,27 +1,9 @@
-import { prisma } from "./prisma";
-import fastify  from 'fastify'
-import { z } from 'zod'
+import fastify from "fastify"
+import { register } from "./http/controllers/register"
+
 
 export const app = fastify()
 
 
 
-app.post('/users', async (request, reply) => {
-    const registerBodySchema = z.object({
-        name: z.string(),
-        email: z.string().email(),
-        password: z.string().min(6)
-    })
-
-    const { name, email, password } = registerBodySchema.parse(request.body)
-
-    const user = await prisma.user.create({
-        data: {
-            name,
-            email,
-            password_hash: password
-        }
-    })
-
-    return reply.status(201).send()
-})
+app.post('/users', register)
